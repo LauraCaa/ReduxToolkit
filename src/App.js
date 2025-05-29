9fa165d73f9ce2339c96bdf5a5fbb53ec930d104
Camila <cardenascastrolauracamila7@gmail.com>
2025-05-28 20:38:26 -0500

    feat: integrate AddTodo and TodoList components into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Counter from "./features/counter/Counter";
+import AddTodo from "./features/todo/AddTodo";
+import TodoList from "./features/todo/TodoList";
 
 function App() {
   return (
     <div>
       <Counter/>
+      <hr/>
+      <div className="todoWrapper">
+        <AddTodo/>
+        <TodoList/>
+      </div>
     </div>
   );
 }
